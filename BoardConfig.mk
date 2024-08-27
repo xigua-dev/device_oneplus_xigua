c26acaf6f0d353c14fde391147074596dc09e03e
Aragaki <4ragaki@gmail.com>
2024-08-27 10:36:33 +0800

    Revert "xigua: use prebuilt kernel"
    This reverts commit 4284a14b90da7adda63a8535231608c83403ce30.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,19 +20,7 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.ir-V1-ndk.so:$(TARGET_COPY_OUT_ODM)/lib64/android.hardware.ir-V1-ndk.so
 
 # Kernel
-TARGET_KERNEL_DIR := $(DEVICE_PATH)-kernel
-KERNEL_MODULE_DIR := $(TARGET_KERNEL_DIR)/modules
-
-BOARD_PREBUILT_DTBIMAGE_DIR := $(TARGET_KERNEL_DIR)
-BOARD_PREBUILT_DTBOIMAGE := $(TARGET_KERNEL_DIR)/dtbo.img
-TARGET_PREBUILT_KERNEL := $(TARGET_KERNEL_DIR)/kernel
-
-# Kernel modules
-KERNEL_MODULES := $(wildcard $(KERNEL_MODULE_DIR)/*.ko)
-
-BOARD_VENDOR_KERNEL_MODULES := $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_MODULES_LOAD)))
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)))
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_ARCHIVE := $(KERNEL_MODULE_DIR)/vendor_ramdisk_include.zip
+TARGET_KERNEL_CONFIG += vendor/oplus/xigua.config
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
